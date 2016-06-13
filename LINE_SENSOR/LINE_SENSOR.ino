e34f08652acc88b2766d7dd9080bec3a96929267
raj174 <rajm174@gmail.com>
2016-06-13 12:16:11 +0000

    Add files via upload

---
+++ LINE_SENSOR/LINE_SENSOR.ino
@@ -0,0 +1,24 @@
+class LineSensor
+{
+  public:
+    LineSensor( void );
+    void begin( const int pin1, const int pin2, const int pin3 );
+    void end();
+  private:
+    boolean _destroyed;
+    const uint8_t _pin1;
+    const uint8_t _pin2;
+    const uint8_t _pin3;
+};
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+}
---
+++ LINE_SENSOR/LineSensorClass.ino
@@ -0,0 +1,15 @@
+LineSensor::LineSensor( void )
+{
+  _destroyed = true;
+}
+
+void LineSensor::begin( const int pin1, const int pin2, const int pin3 )
+{
+  _destroyed = false;
+}
+
+void LineSensor::end( void )
+{
+  _destroyed = true;
+}
+
