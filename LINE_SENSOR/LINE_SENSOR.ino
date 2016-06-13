b35dd01ddd713d20258b48f42269a2c0bb3e90a3
raj174 <rajm174@gmail.com>
2016-06-13 13:00:14 +0000

    Add files via upload

--- LINE_SENSOR/LINE_SENSOR.ino
+++ LINE_SENSOR/LINE_SENSOR.ino
@@ -1,24 +1,39 @@
 class LineSensor
 {
   public:
+    bool available( void );
+    int8_t read( void );
+  
     LineSensor( void );
     void begin( const int pin1, const int pin2, const int pin3 );
     void end();
   private:
     boolean _destroyed;
-    const uint8_t _pin1;
-    const uint8_t _pin2;
-    const uint8_t _pin3;
+    uint8_t _pin1;
+    uint8_t _pin2;
+    uint8_t _pin3;
+    int8_t _lastTriggered;
 };
 
+LineSensor lineSensor;
 void setup()
 {
-  // put your setup code here, to run once:
-
+  lineSensor.begin(3, 4, 5);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-
+  //forward
+  if( lineSensor.available() )
+  {
+    switch( lineSensor.read() )
+    {
+      case -1:
+        break;
+      case 0:
+        break;
+      case 1:
+        break;
+    }
+  }
 }
--- LINE_SENSOR/LineSensorClass.ino
+++ LINE_SENSOR/LineSensorClass.ino
@@ -5,11 +5,59 @@ LineSensor::LineSensor( void )
 
 void LineSensor::begin( const int pin1, const int pin2, const int pin3 )
 {
+  _pin1 = pin1;
+  _pin2 = pin2;
+  _pin3 = pin3;
+  _lastTriggered = 0;
+  
+  pinMode( _pin1, INPUT_PULLUP );
+  pinMode( _pin2, INPUT_PULLUP );
+  pinMode( _pin3, INPUT_PULLUP );
+  
   _destroyed = false;
 }
 
 void LineSensor::end( void )
 {
   _destroyed = true;
+}
+
+boolean LineSensor::available()
+{
+  if(_destroyed == true)
+  {
+    return false;
+  }
+  
+  if( digitalRead(_pin1) == LOW )
+  {
+    _lastTriggered = -1;
+  }
+  else if( digitalRead(_pin2) == LOW )
+  {
+    _lastTriggered = 0;
+  }
+  else if( digitalRead(_pin3) == LOW )
+  {
+    _lastTriggered = 1;
+  }
+  else
+  {
+    return false;
+  }
+
+  return true;
+}
+
+int8_t LineSensor::read( void )
+{
+  if(_destroyed == true)
+  {
+    return false;
+  }
+  else
+  {
+    return _lastTriggered;
+  }
 }
 
