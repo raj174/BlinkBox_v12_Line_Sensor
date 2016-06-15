83a6675a2b806237257303376db9e5d98c2d8412
Chibuike Okpaluba <chibike@users.noreply.github.com>
2016-06-15 09:00:20 +0100

    Update LINE_SENSOR.ino

--- LINE_SENSOR/LINE_SENSOR.ino
+++ LINE_SENSOR/LINE_SENSOR.ino
@@ -3,22 +3,28 @@ class LineSensor
   public:
     bool available( void );
     int8_t read( void );
+    void setMode( boolean mode );
+    void calibrate();
   
     LineSensor( void );
-    void begin( const int pin1, const int pin2, const int pin3 );
+    void begin( const uint8_t pin1, const uint8_t pin2, const uint8_t pin3, const uint8_t _sigPin );
     void end();
   private:
     boolean _destroyed;
     uint8_t _pin1;
     uint8_t _pin2;
     uint8_t _pin3;
+    uint8_t _sigPin;
+    boolean _mode;
     int8_t _lastTriggered;
 };
 
 LineSensor lineSensor;
 void setup()
 {
-  lineSensor.begin(3, 4, 5);
+  lineSensor.begin(3, 4, 5, 6);
+  lineSensor.setMode(1);
+  lineSensor.calibrate();
 }
 
 void loop()
