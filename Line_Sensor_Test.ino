ad0cc92bd2fc1729fd6f8aa83305006920f37006
Raj Manandhar <rajm174@gmail.com>
2016-06-16 16:24:25 +0100

    Add files via upload

---
+++ Line_Sensor_Test.ino
@@ -0,0 +1,28 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5,INPUT_PULLUP);
+  pinMode(7,INPUT_PULLUP);
+  pinMode(8,INPUT_PULLUP);
+  pinMode(10,INPUT_PULLUP);
+  pinMode(12,INPUT_PULLUP);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int v = digitalRead(5);
+  int w = digitalRead(7);
+  int x = digitalRead(8);
+  int y = digitalRead(10);
+  int z = digitalRead(12);
+  Serial.print(v);
+  Serial.print(",");
+  Serial.print(w);
+  Serial.print(",");
+  Serial.print(x);
+  Serial.print(",");
+  Serial.print(y);
+  Serial.print(",");
+  Serial.println(z);
+}
